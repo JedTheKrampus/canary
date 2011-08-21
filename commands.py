b143b634fdda90c019c1d2478c3d5e1fc2e33797
Spencer Brown <sbrown655@gmail.com>
2011-08-21 16:02:39 -0600

    Preparing for the big merge.

--- commands.py
+++ commands.py
@@ -19,4 +19,13 @@ def send(phonebook, voice, args):
         help()
 
 def help():
-    print "Help logic goes here."
+    print """Canary detects that you don't know what you are doing.
+
+Use the send command in the canary shell to send a mass text to the populace of
+New Mexico Tech. To do this, start canary.py and type a command like this:
+
+Canary> send "New Mexico Tech students: Meet in front of Presidents' Hall for an El Camino run."
+
+Then press enter. Canary will proceed to send out your mass text to all the
+students who have signed up to receive Canary text messages. Students may have
+opted in to delay texts or not receive them at all at certain times of day."""
