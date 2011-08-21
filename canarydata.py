5980b8980c7c7679a5fe65f9230d20965e2b8ab3
Arctem <rarctem@gmail.com>
2011-08-21 12:33:27 -0600

    Will now properly create a blank phonebook file if needed.

--- canarydata.py
+++ canarydata.py
@@ -3,25 +3,36 @@ canarydata.py
 #   Created on Sun Aug 21 2011 11:24 GMT -0600 by Arctem
 '''
 import os.path
+import pickle
 
-phonebookdir = '/dat/phonebook.candat'
+phonebookdir = 'dat/phonebook.candat'
 phonebook = []
 
 def load_phonebook():
-    ensure_dir('dat')
+    global phonebook
+    ensure_dir('dat/')
     if not os.path.exists(phonebookdir):
         phonebook_file = open(phonebookdir, 'w')
-        pickle.dump(phonebook, phonebook_file)
-        phonebook_file.close()
-    open(phonebookdir, 'r')
+        try:
+            pickle.dump(phonebook, phonebook_file, 2)
+            phonebook_file.close()
+        except:
+            phonebook_file.close()
+            os.remove(phonebookdir)
+            print 'fatal: Phonebook file could not be found or created.'
+            exit()
+    phonebook_file = open(phonebookdir, 'r')
     phonebook = pickle.load(phonebook_file)
     phonebook_file.close()
+    print phonebook
 
     
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
+
+load_phonebook()
 
 class Student:
     name = ''
