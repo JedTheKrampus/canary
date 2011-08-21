37130eaa2e586bfe7d88caf805242d46caa3d80f
Izzy Cecil <lorr.cecil@gmail.com>
2011-08-21 11:19:43 -0600

    SHELL This is the bassic shell. It reffers to a command file not yet written, and I have omitted the actual import. Main funtion next to come

--- canary.py
+++ canary.py
@@ -9,11 +9,30 @@ from googlevoice import Voice, input
 
 email = ''
 password = ''
-
+phonebook = {}
 voice = Voice()
 
 def shell():
     while True:
         try:
             command = input('Canary>')
-            command = 
+            command = command.lower().split()
+            if command[0] in ('quit','exit'):
+                return
+            if command[0] in ('ls','list'):
+                print dir(commands)
+                continue
+            if command[0] in ('clear','cls'):
+                os.system(['clear','cls'][os.name=='nt'])
+                continue
+            getattr(commands,command[0],commands.not_found)(voice, phonebook,command)
+        except (KeyboardInterrupt, SystemExit):
+            print '\n'
+            print '\aProcess Cancelled'
+            print
+        except:
+            print ('\aAn Error Occured:')
+            print_exc()
+            print
+
+            
