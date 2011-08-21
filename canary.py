e410f92a71d3a49c48f92efdc961c2a6763e8727
Izzy Cecil <lorr.cecil@gmail.com>
2011-08-21 11:08:24 -0600

    Initial commit... 2?

---
+++ canary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+canary.py
+#   Created on Sun Aug 21 2011 11:01 GMT -0600 by Izzy Cecil & Fuzzo & Arctem
+'''
+
+from googlevoice import Voice, input
+
+email = ''
+password = ''
+
+voice = Voice()
+
+def shell():
+    while True:
+        try:
+            command = input('Canary>')
+            command = 
