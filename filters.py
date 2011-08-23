e2d8aa69327e4ef225db28482a91f7b0c895ba1b
Spencer David Brown <sbrown00@nmt.edu>
2011-08-23 14:54:24 -0600

    Added file filters.py for the creation and persistence of filters in an object-oriented manner.

---
+++ filters.py
@@ -0,0 +1,10 @@
+'''
+filters.py
+This file contains code to create a list of students to be texted according to a
+user-defined filter.
+
+Filters will be able to be pickled for later use.
+
+Created on 8-23-2011 by Fuzzo
+'''
+
